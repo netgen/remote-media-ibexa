e623ba34d802c31c1d2628206ab500173ff7a2c2
Randy Čupić <randy@netgen.io>
2024-10-02 12:51:18 +0200

    Force JPG format for image preview

--- bundle/Templating/Twig/Runtime/RemoteMediaIbexaRuntime.php
+++ bundle/Templating/Twig/Runtime/RemoteMediaIbexaRuntime.php
@@ -6,6 +6,7 @@ namespace Netgen\Bundle\RemoteMediaIbexaBundle\Templating\Twig\Runtime;
 
 use Netgen\RemoteMedia\API\ProviderInterface;
 use Netgen\RemoteMedia\API\Values\AuthToken;
+use Netgen\RemoteMedia\API\Values\RemoteResource;
 use Netgen\RemoteMedia\API\Values\RemoteResourceLocation;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
@@ -24,10 +25,14 @@ class RemoteMediaIbexaRuntime
             ? $remoteResourceLocation
             : $this->remoteMediaProvider->authenticateRemoteResourceLocation($remoteResourceLocation, AuthToken::fromDuration(600));
 
+        $isImage = $remoteResourceLocation->getRemoteResource()->getType() === RemoteResource::TYPE_IMAGE;
+        $variationName = $canRead ? 'preview' : 'preview_protected';
+        $variationName = $isImage ? $variationName . '_image' : $variationName;
+
         return $this->remoteMediaProvider->generateVariationHtmlTag(
             location: $remoteResourceLocation,
             variationGroup: 'ibexa_admin',
-            variationName: $canRead ? 'preview' : 'preview_protected',
+            variationName: $variationName,
             forceVideo: true,
             useThumbnail: !$canRead,
         );
